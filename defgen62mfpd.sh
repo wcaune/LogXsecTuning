d0989847d3afabae73371ac6975a66a364d8c511
Biao Wang <biaow@fnal.gov>
2025-03-14 12:30:40 -0400

    defgen for miniproduction 6.2 mfp down

---
+++ defgen62mfpd.sh
@@ -0,0 +1,5 @@
+defgen -r R25-02-27-prod6genie.b -d nd -m genie --tiers genie --use-nova-release --missing-nova-standard -f nonswap --genietune N24_20i_02_11b -h fhc --special miniprod6-2_geniephyopt_mfpdown DEFINE   
+Dataset definition 'prod_genie_R25-02-27-prod6genie.b_nd_genie_N24_20i_02_11b_nonswap_fhc_nova_v08_full_v1_miniprod6-2_geniephyopt_mfpdown' has been created with id 11123915
+
+defgen -r R25-02-27-prod6genie.b -d nd -m genie --tiers genie --use-nova-release --missing-nova-standard -f nonswap --genietune N24_20i_02_11b -h rhc --special miniprod6-2_geniephyopt_mfpdown DEFINE
+Dataset definition 'prod_genie_R25-02-27-prod6genie.b_nd_genie_N24_20i_02_11b_nonswap_rhc_nova_v08_full_v1_miniprod6-2_geniephyopt_mfpdown' has been created with id 11123859
